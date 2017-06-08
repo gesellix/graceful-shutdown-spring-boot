6f9433f06ce2a16b50ea9b0ca5e533daa6d553d1
Tobias Gesellchen <tobias@gesellix.de>
2017-06-08 13:31:31 +0200

    add info contributor to show docker info details.

---
+++ src/main/kotlin/de/gesellix/docker/zerodowntime/DockerInfoContributor.kt
@@ -0,0 +1,22 @@
+package de.gesellix.docker.zerodowntime
+
+import de.gesellix.docker.client.DockerClient
+import de.gesellix.docker.client.DockerClientImpl
+import org.springframework.boot.actuate.info.Info
+import org.springframework.boot.actuate.info.InfoContributor
+import org.springframework.stereotype.Component
+
+@Component
+class DockerInfoContributor : InfoContributor {
+
+    internal val dockerClient: DockerClient = DockerClientImpl()
+
+    override fun contribute(builder: Info.Builder) {
+        if (!dockerClient.ping()?.status?.success!!) {
+            builder.withDetail("docker", "not available (ping failed)")
+        } else {
+            val info = dockerClient.info()?.content
+            builder.withDetail("docker", info)
+        }
+    }
+}
